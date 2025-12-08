e1df1dbc8be7388dc5b4ac0439c1a5f883f77e9a
avgduck <avgduck@gmail.com>
2025-12-08 05:10:57 -0500

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace CharacterStockIcons;
+
+[BepInPlugin(GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("fr.glomzubuk.plugins.llb.llbml", BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency("no.mrgentle.plugins.llb.modmenu", BepInDependency.DependencyFlags.SoftDependency)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string GUID = "avgduck.plugins.llb.tourneyinfo";
+    public static Plugin Instance { get; private set; }
+    internal static ManualLogSource LogGlobal { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        LogGlobal = this.Logger;
+    }
+}
