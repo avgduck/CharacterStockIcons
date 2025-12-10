f24df87e8ba760435001c9c91742da07684596b1
avgduck <avgduck@gmail.com>
2025-12-09 22:37:18 -0500

    Fix wrong GUID

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -8,11 +8,14 @@ using LLBML.Utils;
 namespace CharacterStockIcons;
 
 [BepInPlugin(GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-[BepInDependency("fr.glomzubuk.plugins.llb.llbml", BepInDependency.DependencyFlags.HardDependency)]
-[BepInDependency("no.mrgentle.plugins.llb.modmenu", BepInDependency.DependencyFlags.SoftDependency)]
+[BepInDependency(DEPENDENCY_LLBML, BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency(DEPENDENCY_MODMENU, BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BaseUnityPlugin
 {
-    public const string GUID = "avgduck.plugins.llb.tourneyinfo";
+    public const string GUID = "avgduck.plugins.llb.characterstockicons";
+    internal const string DEPENDENCY_LLBML = "fr.glomzubuk.plugins.llb.llbml";
+    internal const string DEPENDENCY_MODMENU = "no.mrgentle.plugins.llb.modmenu";
+    
     public static Plugin Instance { get; private set; }
     internal static ManualLogSource LogGlobal { get; private set; }
 
